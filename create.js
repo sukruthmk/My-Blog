84c0e02d6a571c88e21a2284607b0c11a61e5d33
Sukruth M K <suk_mallur@yahoo.co.in>
2019-07-27 14:26:14 -0700

    updated create script

--- create.js
+++ create.js
@@ -1,5 +1,4 @@
 const prompt = require('prompt');
-const mkdirp = require('mkdirp');
 const moment = require('moment');
 const _ = require('underscore.string');
 const yaml = require('js-yaml');
@@ -8,20 +7,19 @@ const fs = require('fs');
 prompt.start();
 
 /*eslint-disable */
-prompt.get(['title', 'path', 'category', 'description'], (err, result) => {
+prompt.get(['title', 'category', 'description'], (err, result) => {
   'use strict';
-  const dir = `./content/posts/${moment().format('YYYY-MM-DD')}-${_.slugify(result.title)}`;
-  mkdirp.sync(dir);
-
+  const dir = `./content/posts/${moment().format('YYYY-MM-DD')}---${_.slugify(result.title)}`;
   let postFileStr = '---\n';
 
   const frontmatter = {
     title: result.title,
     date: moment().toJSON(),
-    layout: 'post',
+    template: 'post',
     draft: true,
     slug: `posts/${_.slugify(result.title)}/`,
     category: result.category,
+    tags: "",
     description: result.description
   };
 
